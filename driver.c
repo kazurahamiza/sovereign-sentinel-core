ffd13667d34d3c8f092ac3e2067e027a3042d16c
万国规划师 <kazurahamiza@gmail.com>
2026-08-02 18:18:54 +0800

    Add communication port initialization in driver

--- driver.c
+++ driver.c
@@ -1,46 +1,46 @@
+// driver.c - Add Communication Port initialization
 #include <fltKernel.h>
 
-PFLT_FILTER gFilterHandle = NULL;
+PFLT_PORT g_ServerPort = NULL;
+PFLT_PORT g_ClientPort = NULL;
 
-FLT_PREOP_CALLBACK_STATUS PreCreateCallback(
-    _Inout_ PFLT_CALLBACK_DATA Data,
-    _In_ PCFLT_RELATED_OBJECTS FltObjects,
-    _Flt_CompletionContext_Outptr_ PVOID *CompletionContext
+NTSTATUS ConnectNotifyCallback(
+    IN PFLT_PORT ClientPort,
+    IN PVOID ServerPortCookie,
+    IN PVOID ConnectionContext,
+    IN ULONG SizeOfContext,
+    OUT PVOID *ConnectionCookie
 ) {
-    UNREFERENCED_PARAMETER(FltObjects);
-    UNREFERENCED_PARAMETER(CompletionContext);
-
-    // TODO: Pass file path/buffer to scanner engine
-    // Return FLT_PREOP_DISALLOW_FASTIO or change status to block execution
-
-    return FLT_PREOP_SUCCESS_NO_CALLBACK;
+    g_ClientPort = ClientPort;
+    return STATUS_SUCCESS;
 }
 
-CONST FLT_OPERATION_REGISTRATION Callbacks[] = {
-    { IRP_MJ_CREATE, 0, PreCreateCallback, NULL },
-    { IRP_MJ_OPERATION_END }
-};
+VOID DisconnectNotifyCallback(IN PVOID ConnectionCookie) {
+    FltCloseClientPort(g_FilterHandle, &g_ClientPort);
+    g_ClientPort = NULL;
+}
 
-CONST FLT_REGISTRATION FilterRegistration = {
-    sizeof(FLT_REGISTRATION),
-    FLT_REGISTRATION_VERSION,
-    0,
-    NULL,
-    Callbacks,
-    NULL, // DriverUnload
-    NULL, NULL, NULL, NULL, NULL, NULL, NULL
-};
+// In DriverEntry:
+NTSTATUS SetupCommunicationPort(PFLT_FILTER Filter) {
+    OBJECT_ATTRIBUTES oa;
+    UNICODE_STRING portName = RTL_CONSTANT_STRING(L"\\SovereignSentinelPort");
+    PSECURITY_DESCRIPTOR sd = NULL;
 
-NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath) {
-    UNREFERENCED_PARAMETER(RegistryPath);
-    NTSTATUS status;
+    // Create security descriptor for user-space access
+    FltBuildDefaultSecurityDescriptor(&sd, FLT_PORT_ALL_ACCESS);
+    InitializeObjectAttributes(&oa, &portName, OBJ_KERNEL_HANDLE | OBJ_CASE_INSENSITIVE, NULL, sd);
 
-    status = FltRegisterFilter(DriverObject, &FilterRegistration, &gFilterHandle);
-    if (NT_SUCCESS(status)) {
-        status = FltStartFiltering(gFilterHandle);
-        if (!NT_SUCCESS(status)) {
-            FltUnregisterFilter(gFilterHandle);
-        }
-    }
+    NTSTATUS status = FltCreateCommunicationPort(
+        Filter,
+        &g_ServerPort,
+        &oa,
+        NULL,
+        ConnectNotifyCallback,
+        DisconnectNotifyCallback,
+        NULL,
+        1 // Max connections
+    );
+
+    FltFreeSecurityDescriptor(sd);
     return status;
 }
